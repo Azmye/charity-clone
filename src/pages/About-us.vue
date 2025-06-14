1c7108c8718aedea8257095cca1fda7cb17f09a9
Azmye <mhmmdazmye@gmail.com>
2025-06-14 18:35:07 +0800

    feat: add about section

--- src/pages/About-us.vue
+++ src/pages/About-us.vue
@@ -1,7 +1,10 @@
 <script setup lang="ts">
+import SectionAbout from "../components/section-about.vue";
 import SectionHero from "../components/section-hero.vue";
 </script>
 
 <template>
   <SectionHero />
+
+  <SectionAbout />
 </template>
