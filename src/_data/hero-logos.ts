931ec463a87b534f01ea5858697cf96af41f1763
Azmye <mhmmdazmye@gmail.com>
2025-06-14 18:03:15 +0800

    chore: add assets and assets exporter file

---
+++ src/_data/hero-logos.ts
@@ -0,0 +1,16 @@
+export const heroLogos = [
+  {
+    src: new URL(
+      "../assets/images/hero-logos/6265332e63b917130ca8d702_isa-logo-dog-clean.svg",
+      import.meta.url
+    ).href,
+    alt: "isa-logo-dog-clean",
+  },
+  {
+    src: new URL(
+      "../assets/images/hero-logos/6264275680ed1a3a363d12d8_isa-logo-cat-clean.svg",
+      import.meta.url
+    ).href,
+    alt: "isa-logo-dog-clean",
+  },
+];
---
+++ src/_data/social-icons.ts
@@ -0,0 +1,42 @@
+export const socialIcons = [
+  {
+    href: "#",
+    src: new URL(
+      "../assets/images/social-icons/6274433d06989d76edcff015_9055867_youtube_bxl.svg",
+      import.meta.url
+    ).href,
+    alt: "Youtube",
+  },
+  {
+    href: "#",
+    src: new URL(
+      "../assets/images/social-icons/6274449bf840c377311f2e08_9055851_instagram_bxl.svg",
+      import.meta.url
+    ).href,
+    alt: "Instagram",
+  },
+  {
+    href: "#",
+    src: new URL(
+      "../assets/images/social-icons/627443d78de17d2c59c8352c_9055795_facebook_bxl.svg",
+      import.meta.url
+    ).href,
+    alt: "Facebook",
+  },
+  {
+    href: "#",
+    src: new URL(
+      "../assets/images/social-icons/6274440c6a0ab7631dce0cd1_9055888_patreon_bxl.svg",
+      import.meta.url
+    ).href,
+    alt: "Patreon",
+  },
+  {
+    href: "#",
+    src: new URL(
+      "../assets/images/social-icons/627443f9ac91b492e4c220ba_9055800_telegram_bxl.svg",
+      import.meta.url
+    ).href,
+    alt: "Telegram",
+  },
+];
