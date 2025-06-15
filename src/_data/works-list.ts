6cacb2cd7d1aa5a8f6e153380cc277b2c24c50d1
Azmye <mhmmdazmye@gmail.com>
2025-06-15 15:52:28 +0800

    chore: add works-list data

---
+++ src/_data/works-list.ts
@@ -0,0 +1,122 @@
+export const worksList = [
+  {
+    href: "#",
+    title: "Shelter of Victoria Bulbyna",
+  },
+  {
+    href: "#",
+    title: "City of faithful hearts",
+  },
+  {
+    href: "#",
+    title: "Shelter of Natalia Tymoshenko",
+  },
+  {
+    href: "#",
+    title: "Shelter of Jeanne Sass",
+  },
+  {
+    href: "#",
+    title: "Shelter of Victoria Bondarenko",
+  },
+  {
+    href: "#",
+    title: "Sterilization center",
+  },
+  {
+    href: "#",
+    title: "Zoo chance",
+  },
+  {
+    href: "#",
+    title: "Save lives",
+  },
+  {
+    href: "#",
+    title: "City Center for the Treatment of Animals",
+  },
+  {
+    href: "#",
+    title: "Society for the Protection of Animals in Sumy",
+  },
+  {
+    href: "#",
+    title: "The right to life",
+  },
+  {
+    href: "#",
+    title: "4 paws",
+  },
+  {
+    href: "#",
+    title: "The kingdom of cats",
+  },
+  {
+    href: "#",
+    title: "Zvir",
+  },
+  {
+    href: "#",
+    title: "Mercy",
+  },
+  {
+    href: "#",
+    title: "Happy dog",
+  },
+  {
+    href: "#",
+    title: "Give me a paw, friend",
+  },
+  {
+    href: "#",
+    title: "Friend",
+  },
+  {
+    href: "#",
+    title: "Hope",
+  },
+  {
+    href: "#",
+    title: "We are for the right to life",
+  },
+  {
+    href: "#",
+    title: "Planeta",
+  },
+  {
+    href: "#",
+    title: "Second life",
+  },
+  {
+    href: "#",
+    title: "Happy shore",
+  },
+  {
+    href: "#",
+    title: "Retrieverteam",
+  },
+  {
+    href: "#",
+    title: "Shelter of Irina Dobrolyubova",
+  },
+  {
+    href: "#",
+    title: "Matilda",
+  },
+  {
+    href: "#",
+    title: "Life",
+  },
+  {
+    href: "#",
+    title: "Second chance",
+  },
+  {
+    href: "#",
+    title: "SOS",
+  },
+  {
+    href: "#",
+    title: "Best friends",
+  },
+];
