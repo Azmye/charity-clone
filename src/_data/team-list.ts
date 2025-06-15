47fa71eb17ca74061d729fb7efc9608652ab4ed4
Azmye <mhmmdazmye@gmail.com>
2025-06-15 18:41:26 +0800

    chore: add assets for team profile and create data exporter

---
+++ src/_data/team-list.ts
@@ -0,0 +1,110 @@
+export const teamList = [
+  {
+    fullname: "Daryna Deriy",
+    position: "Chairman of the Board",
+    src: new URL(
+      "../assets/images/team/62744ee9fce7fbaf400c22f8_2.jpg",
+      import.meta.url
+    ).href,
+    alt: "Daryna Deriy",
+  },
+  {
+    fullname: "Polina Snisarenko-Kulchytska",
+    position: "Curator of the Foundation",
+    src: new URL(
+      "../assets/images/team/62744edaac91b4183ac27e10_5.jpg",
+      import.meta.url
+    ).href,
+    alt: "Polina Snisarenko-Kulchytska",
+  },
+  {
+    fullname: "Bohdan Kulchytsky",
+    position: "Executive Manager",
+    src: new URL(
+      "../assets/images/team/62744ec97586a996f48f7a85_1.jpg",
+      import.meta.url
+    ).href,
+    alt: "Bohdan Kulchytsky",
+  },
+  {
+    fullname: "Mykhaylo Deriy",
+    position: "Operation Manager",
+    src: new URL(
+      "../assets/images/team/62744eae22e10d8a15f909ef_9.jpg",
+      import.meta.url
+    ).href,
+    alt: "Mykhaylo Deriy",
+  },
+  {
+    fullname: "Ksenia Nikishina",
+    position: "Office Manager",
+    src: new URL(
+      "../assets/images/team/62744e8e05f6c22bac85252a_7.jpg",
+      import.meta.url
+    ).href,
+    alt: "Ksenia Nikishina",
+  },
+  {
+    fullname: "Anna Andriychuk",
+    position: "SMM-manager",
+    src: new URL(
+      "../assets/images/team/62744f0eb7cd3462ef475ebf_6.jpg",
+      import.meta.url
+    ).href,
+    alt: "Anna Andriychuk",
+  },
+  {
+    fullname: "Anastasia Kovalchuk",
+    position: "Director",
+    src: new URL(
+      "../assets/images/team/62744e6761e425c6956af3a5_3.jpg",
+      import.meta.url
+    ).href,
+    alt: "Anastasia Kovalchuk",
+  },
+  {
+    fullname: "Basil Gloo",
+    position: "Web Developer",
+    src: new URL(
+      "../assets/images/team/626ae0bcd92bf48017c14083_basil-square.png",
+      import.meta.url
+    ).href,
+    alt: "Basil Gloo",
+  },
+  {
+    fullname: "Lina Yakobchuk",
+    position: "Designer",
+    src: new URL(
+      "../assets/images/team/626ae01da2a805d176b1606b_Якобчук.jpg",
+      import.meta.url
+    ).href,
+    alt: "Lina Yakobchuk",
+  },
+  {
+    fullname: "Anastasia Yevchenko",
+    position: "Translator",
+    src: new URL(
+      "../assets/images/team/627fc796cf3602b53a7512cf_4.jpg",
+      import.meta.url
+    ).href,
+    alt: "Anastasia Yevchenko",
+  },
+  {
+    fullname: "Office Manager",
+    position: "open vacancy",
+    src: new URL(
+      "../assets/images/team/627fc7fcdc0f8e9cbcc8ffed_isa-logo-dog-clean.svg",
+      import.meta.url
+    ).href,
+    alt: "Office Manager",
+  },
+  {
+    fullname: "SEO specialist",
+    position: "open vacancy",
+    src: new URL(
+      "../assets/images/team/627fc81139e6f5dca2d02054_isa-logo-cat-clean.svg",
+      import.meta.url
+    ).href,
+    alt: "SEO specialist",
+  },
+];
