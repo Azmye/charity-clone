a422a21fbd15fd7c8f2d0b2bccd8b865957eea69
Azmye <mhmmdazmye@gmail.com>
2025-06-15 19:23:17 +0800

    chore: add citizen profile img and create file exporter

---
+++ src/_data/citizen-list.ts
@@ -0,0 +1,128 @@
+export const citizenList = [
+  {
+    src: new URL(
+      "../assets/images/citizens/628398012a964488bb09078d_13.jpg",
+      import.meta.url
+    ).href,
+    alt: "Oleksiy Tritenko",
+    fullname: "Oleksiy Tritenko",
+    position: "theater and film actor",
+  },
+  {
+    src: new URL(
+      "../assets/images/citizens/628397b3f06d2066cfe8e9a2_14.jpg",
+      import.meta.url
+    ).href,
+    alt: "Stanislav Boklan",
+    fullname: "Stanislav Boklan",
+    position: "People's Artist of Ukraine",
+  },
+  {
+    src: new URL(
+      "../assets/images/citizens/627fcdfcb2137dcf7db53e7f_12.jpg",
+      import.meta.url
+    ).href,
+    alt: "Alexander Rudinsky",
+    fullname: "Alexander Rudinsky",
+    position: "theater and film actor",
+  },
+  {
+    src: new URL(
+      "../assets/images/citizens/627fcdd72d729d8ecacf6de7_11.jpg",
+      import.meta.url
+    ).href,
+    alt: "Oksana Zhdanova",
+    fullname: "Oksana Zhdanova",
+    position: "theater and film actress",
+  },
+  {
+    src: new URL(
+      "../assets/images/citizens/627fcdaedc0f8eea17c92429_10.jpg",
+      import.meta.url
+    ).href,
+    alt: "Natalka Denisenko",
+    fullname: "Natalka Denisenko",
+    position: "theater and film actress",
+  },
+  {
+    src: new URL(
+      "../assets/images/citizens/627fcd87d51b9051dfcbf349_09.jpg",
+      import.meta.url
+    ).href,
+    alt: "Maksym Samchyk",
+    fullname: "Maksym Samchyk",
+    position: "theater and film actor",
+  },
+  {
+    src: new URL(
+      "../assets/images/citizens/627fcd5ca9508410b3a0d824_08.jpg",
+      import.meta.url
+    ).href,
+    alt: "Kateryna Vyshneva",
+    fullname: "Kateryna Vyshneva",
+    position: "theater and film actor",
+  },
+  {
+    src: new URL(
+      "../assets/images/citizens/627fcd3288758547e6ee0e22_07.jpg",
+      import.meta.url
+    ).href,
+    alt: "Irma Vitovska",
+    fullname: "Irma Vitovska",
+    position: "Honored Actress of Ukraine",
+  },
+  {
+    src: new URL(
+      "../assets/images/citizens/627fcd1790c298c49bc7c436_06.jpg",
+      import.meta.url
+    ).href,
+    alt: "Igor Portyanko",
+    fullname: "Igor Portyanko",
+    position: "actor of theater and cinema",
+  },
+  {
+    src: new URL(
+      "../assets/images/citizens/627fccf0fc672a5dcfa87ee9_05.jpg",
+      import.meta.url
+    ).href,
+    alt: "Dmytro Surzhikov",
+    fullname: "Dmytro Surzhikov",
+    position: "Honored Artist of Ukraine",
+  },
+  {
+    src: new URL(
+      "../assets/images/citizens/627fccc57c3cb5177369015e_04.jpg",
+      import.meta.url
+    ).href,
+    alt: "Daria Barihashvili",
+    fullname: "Daria Barihashvili",
+    position: "theater and film actress",
+  },
+  {
+    src: new URL(
+      "../assets/images/citizens/627fcc9bc50d7acc036982a9_03.jpg",
+      import.meta.url
+    ).href,
+    alt: "Vera Kobzar",
+    fullname: "Vera Kobzar",
+    position: "theater and film actress",
+  },
+  {
+    src: new URL(
+      "../assets/images/citizens/627fcc7339e6f5389ed03013_02.jpg",
+      import.meta.url
+    ).href,
+    alt: "Andriy Fedynchyk",
+    fullname: "Andriy Fedynchyk",
+    position: "theater and film actor",
+  },
+  {
+    src: new URL(
+      "../assets/images/citizens/627fcc4b694dfd4d65a13230_01.jpg",
+      import.meta.url
+    ).href,
+    alt: "Anastasia Nesterenko",
+    fullname: "Anastasia Nesterenko",
+    position: "theater and film actress",
+  },
+];
