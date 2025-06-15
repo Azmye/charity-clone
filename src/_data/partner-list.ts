00598f8058f6fc0145c1c09bac9d9769efce6bed
Azmye <mhmmdazmye@gmail.com>
2025-06-15 20:46:52 +0800

    chore: add partner logo and data exporter file

---
+++ src/_data/partner-list.ts
@@ -0,0 +1,44 @@
+export const partnerList = [
+  {
+    src: new URL(
+      "../assets/images/partners/62aa3401042d425391235aaa_Logo-adviRES.jpg",
+      import.meta.url
+    ).href,
+    alt: "adviRES",
+  },
+  {
+    src: new URL(
+      "../assets/images/partners/62aa33e1380de680b7110ebf_ovid-wind-logo.png",
+      import.meta.url
+    ).href,
+    alt: "ovid wind",
+  },
+  {
+    src: new URL(
+      "../assets/images/partners/6265d803f32677c8352bc485_tbs_logo1-02.png",
+      import.meta.url
+    ).href,
+    alt: "tbs",
+  },
+  {
+    src: new URL(
+      "../assets/images/partners/6265d7edf0c46ce38f559328_download.png",
+      import.meta.url
+    ).href,
+    alt: "accurl",
+  },
+  {
+    src: new URL(
+      "../assets/images/partners/6265d7d930a5c2377aedf9d4_logo_laudis_accountancy.png",
+      import.meta.url
+    ).href,
+    alt: "laudis",
+  },
+  {
+    src: new URL(
+      "../assets/images/partners/6265d7c51ad96b1cbf31b0c0_photo_2022-04-21 20.43.09.jpeg",
+      import.meta.url
+    ).href,
+    alt: "kickbite",
+  },
+];
